7a14a46924cca49c948dc235d6a0eb5527ec18a0
Viktor Nova <viktor@pinknoiseboys.com>
2023-10-11 05:33:41 -0700

    Rearrange header layout

--- packages/theme_basic_bedrock/themes/basic_bedrock/elements/header.php
+++ packages/theme_basic_bedrock/themes/basic_bedrock/elements/header.php
@@ -7,33 +7,24 @@ $view->inc('elements/header_top.php');
 <header>
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-5">
                 <?php
                 $a = new GlobalArea('Logo');
                 $a->setBlockLimit(1);
                 $a->display();
                 ?>
             </div>
-            <div class="col-md-6">
-                <?php
-                $a = new GlobalArea('Top Right');
-                $a->setBlockLimit(1);
-                $a->display();
-                ?>
+			<div class="col-md-7 nav-container container">
+				<nav class="navbar fixed-top">
+			        <?php
+			            $a = new GlobalArea('Navigation');
+			            $a->setBlockLimit(1);
+			            $a->display($c);
+			            ?>
+				</nav>
             </div>
         </div>
     </div>
 </header>
 
 
-
-<div class="nav-container container">
-	<nav class="navbar fixed-top">
-        <?php
-            $a = new GlobalArea('Navigation');
-            $a->setBlockLimit(1);
-            $a->display($c);
-            ?>
-
-	</nav>
-</div>
